8e3fea8fc9bc149b3aa48cd8be17f3d4d219ffcf
Hall A Online <a-onl@jlab.org>
2018-10-19 18:16:03 -0400

    update odef files from the latest SC exp directory

--- replay/rootlogon.C
+++ replay/rootlogon.C
@@ -31,8 +31,9 @@
     gSystem->Load(Form(replay_dir_prefix,"ReplayCore64_C.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/Tritium_Xscin/libTritium_Xscin.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcScin/libTriFadcScin.so"));
-    gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcCherenkov/libTriFadcCherenkov.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcXscin/libTriFadcXscin.so"));
+    gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcCherenkov/libTriFadcCherenkov.so"));
+    gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcShower/libTriFadcShower.so"));
     gSystem->Load(Form("./libraries/Tritium_HRS/libTritium_HRS.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/Tritium_TSScaler/libTritium_TSScaler.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/Tritium_Evt100/libTritium_THaScaler100EvtHandler.so"));
@@ -40,14 +41,16 @@
     gSystem->Load(Form(replay_dir_prefix,"libraries/Tritium_Track_Eloss/libTritium_Track_Eloss.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriScalerEvtHandler/libTriScaler.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcRasteredBeam/libTriFadcRasteredBeam.so"));
+    gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcUnRasteredBeam/libTriFadcUnRasteredBeam.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriVDCeff/libTriVDCeff.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriOldTrack/libTriOldTrack.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriBCM/libTriBCM.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriF1VETROC/libVETROCF1tdc_tritium.so")); 
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriV1495/libV1495_tritium.so")); 
-    gSystem->Load(Form(replay_dir_prefix,"libraries/Tri_ElossCorrection/libTri_ElossCorrection.so"));
-    gSystem->Load(Form(replay_dir_prefix,"libraries/Tri_Beam_Eloss/libTri_Beam_Eloss.so"));
-    gSystem->Load(Form(replay_dir_prefix,"libraries/Tri_Track_Eloss/libTri_Track_Eloss.so"));
+    gSystem->Load(Form(replay_dir_prefix,"libraries/Tri_ElossCorrection/libTri_ElossCorrection.so")); 
+    gSystem->Load(Form(replay_dir_prefix,"libraries/Tri_Beam_Eloss/libTri_Beam_Eloss.so")); 
+    gSystem->Load(Form(replay_dir_prefix,"libraries/Tri_Track_Eloss/libTri_Track_Eloss.so")); 
+
   }
 
     //Load more libs here, if necessary. 
