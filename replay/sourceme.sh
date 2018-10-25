c95829d0794cf1b7356c3e46bc63b7285dcff366
itabasi <itabashi@ifarm1402.jlab.org>
2018-10-25 14:29:17 -0400

    modify PATH in ifarm

--- replay/sourceme.sh
+++ replay/sourceme.sh
@@ -1,5 +1,9 @@
 #export LD_LIBRARY_PATH=/adaqfs/home/a-onl/tritium/analyzer:/adaqfs/home/a-onl/evio-4.3/Linux-x86_64/lib/:$ROOTSYS/lib
 #export PATH=/adaqfs/home/a-onl/tritium/analyzer/:$PATH
-export DB_DIR=/adaqfs/home/a-onl/tritium/HallA-Online-Tritium/replay/DB/
-
+#export DB_DIR=/adaqfs/home/a-onl/tritium/HallA-Online-Tritium/replay/DB/
 #export ANALYZER=/adaqfs/home/a-onl/tritium/analyzer/
+
+setenv LD_LIBRARY_PATH /u/home/itabashi/analyzer-1.6.0:$ROOTSYS/lib
+setenv PATH /u/home/itabashi/analyzer-1.6.0/:$PATH
+setenv DB_DIR /adaqfs/home/itabashi/nnL/HallA-Online-Tritium/replay/DB/
+setenv ANALYZER /u/home/itabashi/analyzer-1.6.0/
